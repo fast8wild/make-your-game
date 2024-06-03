c7feb9ba17db4b38c9502e00e721da8767afc91e
ammsaeed <noammar.ammar@gmail.com>
2024-06-03 17:21:01 +0300

    functional. I just wanna add more robust brick collision

--- breakout.js
+++ breakout.js
@@ -2,36 +2,35 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 let leftPressed = false
 let rightPressed = false
-let serveState = true // Start of the game, press enter to launch the ball
-let endState = false // Game over state
-let winState = false
+let gameState = 1; //-1: Game Over 0: In-Play, 1: Serve, 2: Win]
 
 let startTime = new Date().getTime(); // Start of serve time
 let prevTime = 0 // Add to the time counter on retries
 
-let ui = {
+const ui = {
   time: "00:00",
   lives: 3,
   score: 0,
+  level: 1,
 }
 
-let paddle = {
+const paddle = {
   w: 80, // width
   h: 15, // height
-  x: canvas.width/2 - 80/2, // top left x
+  x: canvas.width / 2 - 80 / 2, // top left x
   y: canvas.height - 15 - 5, // top left y
   dx: 4,
 }
 
-let ball = {
+const ball = {
   r: 10, // radius
-  x: paddle.x + paddle.w/2, // center x
+  x: paddle.x + paddle.w / 2, // center x
   y: paddle.y - 10, // center y
   dx: 2,
   dy: -2
 }
 
-let brickSettings = {
+const brickSettings = {
   rowCount: 3,
   colCount: 5,
   w: 75,
@@ -49,7 +48,10 @@ function initializeBricks() {
   for (let c = 0; c < brickSettings.colCount; c++) {
     bricks[c] = [];
     for (let r = 0; r < brickSettings.rowCount; r++) {
-      bricks[c][r] = { x: 0, y: 0, health: 1 };
+      const brickX = c * (brickSettings.w + brickSettings.padding) + brickSettings.offsetLeft;
+      const brickY = r * (brickSettings.h + brickSettings.padding) + brickSettings.offsetTop;
+      bricks[c][r] = { x: brickX, y: brickY, health: Math.min(3,Math.floor(Math.random()*(ui.level+1))) };
+      console.log(bricks[c][r].health)
     }
   }
 }
@@ -65,7 +67,7 @@ function drawBall() {
 }
 
 function drawPaddle() {
-  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)  
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
   ctx.fillStyle = "#8895DD";
   ctx.fill();
 }
@@ -73,14 +75,21 @@ function drawPaddle() {
 function drawBricks() {
   for (let c = 0; c < brickSettings.colCount; c++) {
     for (let r = 0; r < brickSettings.rowCount; r++) {
-      if (bricks[c][r].health > 0) {
-        const brickX = c * (brickSettings.w + brickSettings.padding) + brickSettings.offsetLeft;
-        const brickY = r * (brickSettings.h + brickSettings.padding) + brickSettings.offsetTop;
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
+      const brick = bricks[c][r]
+      if (brick.health > 0) {
         ctx.beginPath();
-        ctx.rect(brickX, brickY, brickSettings.w, brickSettings.h);
-        ctx.fillStyle = "#0095DD";
+        ctx.rect(brick.x, brick.y, brickSettings.w, brickSettings.h);
+        switch (brick.health) {
+          case 3:
+            ctx.fillStyle = "#DD0095";
+            break;
+          case 2:
+            ctx.fillStyle = "#95DD00";
+            break;
+          default:
+            ctx.fillStyle = "#0095DD";
+            break;
+        }
         ctx.fill();
         ctx.closePath();
       }
@@ -94,9 +103,9 @@ function drawGameOver() {
   ctx.fillStyle = "Red"
   ctx.textAlign = "Center"
   ctx.textBaseLine = "Middle"
-  ctx.fillText(text,canvas.width/2-ctx.measureText(text).width/2, canvas.height/2)
+  ctx.fillText(text, canvas.width / 2 - ctx.measureText(text).width / 2, canvas.height / 2)
   text = "Press Enter to retry"
-  ctx.fillText(text,canvas.width/2-ctx.measureText(text).width/2, canvas.height/2+50)
+  ctx.fillText(text, canvas.width / 2 - ctx.measureText(text).width / 2, canvas.height / 2 + 50)
 }
 
 function drawWin() {
@@ -105,10 +114,10 @@ function drawWin() {
   ctx.fillStyle = "Green"
   ctx.textAlign = "Center"
   ctx.textBaseLine = "Middle"
-  ctx.fillText(text,canvas.width/2-ctx.measureText(text).width/2, canvas.height/2)
+  ctx.fillText(text, canvas.width / 2 - ctx.measureText(text).width / 2, canvas.height / 2)
   text = "Press Enter for the next level"
   ctx.font = "30px Arial";
-  ctx.fillText(text,canvas.width/2-ctx.measureText(text).width/2, canvas.height/2+50)
+  ctx.fillText(text, canvas.width / 2 - ctx.measureText(text).width / 2, canvas.height / 2 + 50)
 }
 
 function drawStartMessage() {
@@ -117,16 +126,14 @@ function drawStartMessage() {
   ctx.fillStyle = "Blue"
   ctx.textAlign = "Center"
   ctx.textBaseLine = "Middle"
-  ctx.fillText(text,canvas.width/2-ctx.measureText(text).width/2, canvas.height/2)
+  ctx.fillText(text, canvas.width / 2 - ctx.measureText(text).width / 2, canvas.height / 2)
 }
 
 function drawUI() {
-  if (serveState) {
-    drawStartMessage();
-  } else {
-    let dt = (new Date().getTime() - startTime)/1000 + prevTime
-    var sec = Math.floor(dt%60);
-    var min = Math.floor(dt/60);
+  if (gameState == 0) { // Calcilate time only when the ball is in-play
+    let dt = (new Date().getTime() - startTime) / 1000 + prevTime
+    var sec = Math.floor(dt % 60);
+    var min = Math.floor(dt / 60);
     ui.time = (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec);
   }
   ctx.font = "20px Arial";
@@ -134,41 +141,49 @@ function drawUI() {
   ctx.lineWidth = 0.1
   ctx.strokeStyle = "white"
   // Time
-  ctx.fillText("Time: "+ui.time,1,20)
-  ctx.strokeText("Time: "+ui.time,1,20)
+  ctx.fillText("Time: " + ui.time, 1, 20)
+  ctx.strokeText("Time: " + ui.time, 1, 20)
   // Lives
-  ctx.fillText("Lives: "+ui.lives,1,40)
-  ctx.strokeText("Lives: "+ui.lives,1,40)
+  ctx.fillText("Lives: " + ui.lives, 1, 40)
+  ctx.strokeText("Lives: " + ui.lives, 1, 40)
+  // Level
+  ctx.fillText("Level: " + ui.level, 1, 60)
+  ctx.strokeText("Level: " + ui.level, 1, 60)
   // Score
-  ctx.fillText("Score: "+ui.score,1,60)
-  ctx.strokeText("Score: "+ui.score,1,60)
+  ctx.fillText("Score: " + ui.score, 1, 80)
+  ctx.strokeText("Score: " + ui.score, 1, 80)
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  if (endState) {
-    drawGameOver();
-    return;
+  switch (gameState) {
+    case -1: // Game Over
+      drawGameOver();
+      break;
+    case 1: // Serve
+      drawStartMessage();
+    case 0: //In-play
+      drawPaddle()
+      drawBall();
+      drawBricks();
+      drawUI();
+      updatePaddle();
+      updateBall();
+      break;
+    case 2:
+      drawWin()
+      break;
   }
-  if (winState) {
-    drawWin();
-    return;
-  }
 
-  drawPaddle()
-  drawBall();
-  drawBricks();
-  drawUI();
-  updatePaddle();
-  updateBall();
-  
   requestAnimationFrame(draw);
 }
 
+function drawActiveGame() {
+}
+
 function updateBall() {
-  if (serveState) {
-    ball.x = paddle.x + paddle.w/2
+  if (gameState == 1) { // Stick the ball to the paddle on serve
+    ball.x = paddle.x + paddle.w / 2
     ball.y = paddle.y - 10
   } else {
     ball.x += ball.dx;
@@ -181,56 +196,56 @@ function updateBall() {
 
 function updatePaddle() {
   if (leftPressed) {
-    paddle.x = Math.max(0,paddle.x-paddle.dx)
+    paddle.x = Math.max(0, paddle.x - paddle.dx)
   }
   if (rightPressed) {
-    paddle.x = Math.min(canvas.width-paddle.w,paddle.x+paddle.dx)
+    paddle.x = Math.min(canvas.width - paddle.w, paddle.x + paddle.dx)
   }
 }
 
 function screenCollission() {
   if (ball.x >= canvas.width - ball.r) { // Right screen collision
     ball.dx *= -1
-    ball.x = 2*(canvas.width - ball.r) - ball.x
+    ball.x = 2 * (canvas.width - ball.r) - ball.x
   } else if (ball.x <= ball.r) { // Left screen collision
     ball.dx *= -1
-    ball.x = 2*ball.r-ball.x
+    ball.x = 2 * ball.r - ball.x
   }
   if (ball.y <= ball.r) { // Top screen collision
     ball.dy *= -1
-    ball.y = 2*ball.r-ball.y
+    ball.y = 2 * ball.r - ball.y
   } else if (ball.y >= canvas.height + ball.r) { //Bottom screen collision
     // lives should subtract, game should reset
     ui.lives--
     if (ui.lives > 0) {
-      prevTime += (new Date().getTime() - startTime)/1000
-      serveState = true
+      prevTime += (new Date().getTime() - startTime) / 1000
+      gameState = 1
       ball.dx = 2
       ball.dy = -2
-      ball.x = paddle.x + paddle.w/2
+      ball.x = paddle.x + paddle.w / 2
       ball.y = paddle.y - ball.r
     } else {
-      endState = true
+      gameState = -1
     }
   }
 }
 
 function paddleCollission() {
   if ( //Bottom paddle collision
-    ball.dy > 0 && 
-    ball.y + ball.r >= paddle.y && 
-    ball.y + ball.r <= paddle.y+tolerance && 
-    ball.x + ball.r >= paddle.x && 
+    ball.dy > 0 &&
+    ball.y + ball.r >= paddle.y &&
+    ball.y + ball.r <= paddle.y + tolerance &&
+    ball.x + ball.r >= paddle.x &&
     ball.x - ball.r <= paddle.x + paddle.w) {
     ball.dy *= -1
     // speed multiplyer on edge hits: 
-    let diff = Math.max(0.7,Math.abs(ball.x - paddle.x - paddle.w/2)/(paddle.w/2)) // How far away it is from the center from 0 to 1
-    ball.dx > 0 ? ball.dx = diff*3 : ball.dx = -diff*3;
-    ball.dy = -diff*3
+    let diff = Math.max(0.7, Math.abs(ball.x - paddle.x - paddle.w / 2) / (paddle.w / 2)) // How far away it is from the center from 0 to 1
+    ball.dx > 0 ? ball.dx = diff * 3 : ball.dx = -diff * 3;
+    ball.dy = -diff * 3
 
   } else if ( // Side paddle collission
-    ball.y + ball.r > paddle.y+tolerance && 
-    ball.x + ball.r >= paddle.x && 
+    ball.y + ball.r > paddle.y + tolerance &&
+    ball.x + ball.r >= paddle.x &&
     ball.x - ball.r <= paddle.x + paddle.w) {
     ball.dx *= -1
   }
@@ -243,12 +258,12 @@ function brickCollission() {
       const brick = bricks[c][r];
       if (brick.health > 0) {
         if (
-          ball.x > brick.x &&
-          ball.x < brick.x + brickSettings.w &&
-          ball.y > brick.y &&
-          ball.y < brick.y + brickSettings.h
+          ball.x >= brick.x &&
+          ball.x <= brick.x + brickSettings.w &&
+          ball.y >= brick.y &&
+          ball.y <= brick.y + brickSettings.h
         ) {
-          ui.score += 10*brick.health
+          ui.score += 10 * brick.health * ui.level
           ball.dy *= -1;
           brick.health--;
         } else {
@@ -258,8 +273,8 @@ function brickCollission() {
     }
   }
   if (levelClear) {
-    prevTime += (new Date().getTime() - startTime)/1000
-    winState = true
+    prevTime += (new Date().getTime() - startTime) / 1000
+    gameState = 2
   }
 }
 
@@ -272,22 +287,24 @@ function keyDownHandler(e) {
       leftPressed = true;
       break;
     case "Enter":
-      if (endState) {
-        initializeBricks();
-        ui.lives = 3
-        ui.score = 0
-        ui.time = "0:00"
-        serveState = true
-        endState = false
-        prevTime = 0
-      } if (winState) {
-        initializeBricks();
-        winState = false
-        serveState = true
-        endState = false
-      } else {
-        serveState = false
-        startTime = new Date().getTime()
+      switch (gameState) {
+        case -1: // Reset from game over
+          initializeBricks();
+          gameState = 1;
+          ui.lives = 3
+          ui.score = 0
+          ui.time = "0:00"
+          ui.level = 1
+          prevTime = 0
+          break;
+        case 2: // Win state, reset the board and go back to serve
+          initializeBricks();
+          gameState = 1;
+          ui.level++
+          break;
+        case 1: // Serve the ball
+          gameState = 0
+          startTime = new Date().getTime()
       }
       break;
   }
